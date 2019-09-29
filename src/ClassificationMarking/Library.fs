153c88d6206ee4dcbd6e1025e08e5db93c52d413
Bennie Copeland <bennie.r.copeland@gmail.com>
2019-09-29 10:52:25 +0200

    WIP

--- src/ClassificationMarking/Library.fs
+++ src/ClassificationMarking/Library.fs
@@ -1,5 +1,6 @@
 ﻿namespace ClassificationMarking
 
+
 module Say =
     let hello name =
         printfn "Hello %s" name
@@ -19,40 +20,141 @@ module Say =
 // portion mark
 
     // double slashes separate
-    type Designator =
+    type Trigraph = Trigraph of string
+    type Tetragraph = Tetragraph of string
+    
+    
+    type USDesignator =
         | TopSecret
         | Secret
         | Confidential
         
-    type USClassification = USClassification of Designator
-    type ForeignClassification = ForeignClassification of string
+    
+    
+    type CountryCode = CountryCode of string
+    
+    type ForeignDesignator =
+        | TopSecret
+        | Secret
+        | Confidential
+        | Restricted
+        | Unclassified
+    
     type JointClassification = JointClassification of string 
         
+    type USClassification = {
+        USDesignator: USDesignator
+    }
     
-    
-    type Classified =
-        | USClassification
-        | ForeignClassification
+    type ClassifiedDesignator =
+        | USClassification of USDesignator
+        | ForeignClassification of (CountryCode * ForeignDesignator)
         | JointClassification
         
     type Classification =
         | Unclassified
-        | Classified of Classified
+        | Classified of ClassifiedDesignator
         
     
+    
+    type SCIControlSystem =
+        | HCS
+        | SI
+        | TK
+        | Unpublished of string
+    
+    type SCI = SCI of string
+    type SAP = SAP of string
+    type AEA = AEA of string
+    type FGI = FGI of string
         
-    type ControlMarking = ControlMarking of string
-    type ClassificationAuthorityBlock =
-        | OriginalClassification of string
-        | DerivativeClassification of string
+    type DisseminationMarking =
+        | NOFORN of string
+    
+    type ControlMarking =
+        | SCI of SCI
+        | SAP of SAP
+        | AEA of AEA
+        | FGI of FGI
+        | Dissemination of DisseminationMarking
+    
+    type Marking = {
+        Classification : Classification
+        ControllerMarkings : ControlMarking list
+    }    
         
     type BannerLine = BannerLine of string
     type PortionMark = PortionMark of string
     
+    type Identifier =
+        | PositionTitle of string
+        | Personal of string
+    
+    type ClassifiyingOrganization = {
+        DoDComponent: string
+        OfficeOfOrigin: string
+    }
+    
+    type Date = {
+        Year: int
+        Month: int
+        Day: int
+    }
+    
+    let usClassificationAsBannerString c =
+        match c with
+        | USDesignator.TopSecret -> "TOP SECRET"
+        | USDesignator.Secret -> "SECRET"
+        | USDesignator.Confidential -> "CONFIDENTIAL"
+        
+    let classifiedDesignatorToBannerString c =
+        match c with
+        | USClassification us -> usClassificationAsBannerString us
+        | ForeignClassification (countryCode, designator) -> "FOREIGN"
+        | JointClassification -> "JOINT"
+        
+    let classificationToBannerString c =
+        match c with
+        | Unclassified -> "UNCLASSIFIED"
+        | Classified classified -> classifiedDesignatorToBannerString classified
+        
+    let usClassificationAsPortionString c =
+        match c with
+        | USDesignator.TopSecret -> "TS"
+        | USDesignator.Secret -> "S"
+        | USDesignator.Confidential -> "C"
+    
+    let DateToString d =
+        sprintf "%04i%02i%02i" d.Year d.Month d.Day
+    
+    type ClassifiedBy = {
+        Name : string
+        Identifier: Identifier
+        ClassifyingOrganization : ClassifiyingOrganization option
+    }
+    
+    type OriginalClassification = {
+        ClassifiedBy : ClassifiedBy
+    }
+    
+    type ClassificationAuthorityBlock =
+        | OriginalClassification of OriginalClassification
+        | DerivativeClassification of string
+    
     let CreateBannerLine s =
         // Review Enclosure 3 section 5
-        "value"
+        sprintf "%s" (classificationToBannerString s)
     
     let CreatePortionMark s =
         // Review Enclosure 3 section 6
+        "({classification}//{control markings})"
+        
+    let CreateSpecialNoticies =
         "value"
+        
+    let CreateClassificationAuthorityBlock =
+        // if (RD || FRD) && NSI, "Declassify On:" = "Not Applicable (or N/A) to RD/FRD portions. See source list for NSI portions."
+        // if (RD || FRD) && !NSI, Do not mark with declassification instructions
+        "value"
+        
+    
